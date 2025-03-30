11e3d192e09af40a9cda309527bb943fc7a656d2
Tomasz Woźniak <wozniak.tom@pm.me>
2025-03-31 10:16:11 +1100

    GroupPANELest - the beginnings #10

---
+++ src/bvarGroupPANELest.h
@@ -0,0 +1,23 @@
+#ifndef _BVARGROUPPANELEST_H_
+#define _BVARGROUPPANELEST_H_
+
+#include <RcppArmadillo.h>
+
+using namespace Rcpp;
+using namespace arma;
+
+
+Rcpp::List bvarGroupPANELest(
+    const int&                    S,                    // No. of posterior draws
+    const Rcpp::List&             Y,                    // a C-list of T_cxN elements
+    const Rcpp::List&             X,                    // a C-list of T_cxK elements
+    const Rcpp::List&             prior,                // a list of priors
+    const Rcpp::List&             starting_values, 
+    const int                     thin,                 // introduce thinning
+    const bool                    show_progress,
+    const arma::vec&              adptive_alpha_gamma,  // 2x1 vector with target acceptance rate and step size
+    const bool                    estimate_groups = true // whether to estimate group-specific parameters
+);
+
+
+#endif  // _BVARGROUPPANELEST_H_
