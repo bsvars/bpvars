0f350a27094919d6db9ac22b71e8e13e3b01270b
Tomasz Woźniak <wozniak.tom@pm.me>
2025-04-26 18:59:29 +1000

    updated group Panel Gibbs for estimated allocations #10
    + works
    + compiles
    + requires checking! only one group estimated, but how?

--- src/bvarGroupPANEL.cpp
+++ src/bvarGroupPANEL.cpp
@@ -3,6 +3,7 @@
 #include "bsvars.h"
 
 #include "sample_mniw.h"
+#include "utils.h"
 
 using namespace Rcpp;
 using namespace arma;
@@ -48,7 +49,9 @@ Rcpp::List bvarGroupPANEL(
   cube    aux_Sigma_g = as<cube>(starting_values["Sigma_g"]);
   mat     aux_A       = as<mat>(starting_values["A"]);
   mat     aux_V       = as<mat>(starting_values["V"]);
+  mat     aux_V_inv   = inv_sympd( aux_V );
   mat     aux_Sigma   = as<mat>(starting_values["Sigma"]);
+  mat     aux_Sigma_inv = inv_sympd( aux_Sigma );
   double  aux_nu      = as<double>(starting_values["nu"]);
   double  aux_m       = as<double>(starting_values["m"]);
   double  aux_w       = as<double>(starting_values["w"]);
@@ -68,6 +71,7 @@ Rcpp::List bvarGroupPANEL(
   cube        posterior_A(K, N, SS);
   cube        posterior_V(K, K, SS);
   cube        posterior_Sigma(N, N, SS);
+  mat         posterior_ga(C, SS);
   vec         posterior_nu(SS);
   vec         posterior_nu_S(S);
   vec         posterior_m(SS);
@@ -80,26 +84,20 @@ Rcpp::List bvarGroupPANEL(
     aux_Sigma_g_inv.slice(g) = inv_sympd( aux_Sigma_g.slice(g) );
   }
   
-  field<mat> y(C);
-  field<mat> x(C);
-  field<mat> YG(G);
-  field<mat> XG(G);
-  int nryg = 0;
+  mat y_tmp             = as<mat>(Y[0]);
+  int T                 = y_tmp.n_rows;
+  
+  cube yt(N, T, C);           // store provided data matrices
+  cube xt(K, T, C);           // store provided data matrices
   
   for (int c=0; c<C; c++) {
     
     aux_Sigma_c_inv.slice(c) = aux_Sigma_g_inv.slice(aux_ga(c));
     
-    y(c)                  = as<mat>(Y[c]);
-    x(c)                  = as<mat>(X[c]);
-    
-    nryg                  = YG(aux_ga(c)).n_rows;
-    YG(aux_ga(c)).insert_rows( nryg, y(c) );
-    XG(aux_ga(c)).insert_rows( nryg, x(c) );
+    yt.slice(c)         = trans(as<mat>(Y[c]));
+    xt.slice(c)         = trans(as<mat>(X[c]));
     
   } // END c loop
-  
-  
   vec   scale(S);
   int   ss = 0;
   
@@ -107,7 +105,7 @@ Rcpp::List bvarGroupPANEL(
   // Hessian for the posterior log_kenel for nu
   double adaptive_scale = cov_nu(aux_nu, C, N);
   vec aux_nu_tmp(2);
-
+  
   for (int s=0; s<S; s++) {
     // Rcout << "Iteration: " << s << endl;
     
@@ -127,28 +125,39 @@ Rcpp::List bvarGroupPANEL(
     scale(s)    = aux_nu_tmp(1);
     
     // sample aux_Sigma
-    aux_Sigma   = sample_Sigma( aux_Sigma_g_inv, aux_s, aux_nu, prior );
+    aux_Sigma     = sample_Sigma( aux_Sigma_g_inv, aux_s, aux_nu, prior );
+    aux_Sigma_inv = inv_sympd( aux_Sigma );
     
     // sample aux_A, aux_V
     field<mat> tmp_AV     = sample_AV( aux_A_g, aux_Sigma_g_inv, aux_s, aux_m, aux_w, prior );
     aux_A       = tmp_AV(0);  
     aux_V       = tmp_AV(1);
+    aux_V_inv   = inv_sympd( aux_V );
+    
+    // sample aux_ga
+    if ( estimate_groups ) {
+      aux_ga    = sample_group_allocation ( aux_ga, yt, xt, aux_A_g, aux_Sigma_g, aux_A, aux_V_inv, aux_Sigma, aux_Sigma_inv, aux_nu );
+    }
     
     // sample aux_A_c, aux_Sigma_c
     for (int g=0; g<G; g++) {
-      field<mat> tmp_A_g_Sigma_g  = sample_A_c_Sigma_c( YG(g), XG(g), aux_A, aux_V, aux_Sigma, aux_nu );
+    
+      uvec which_in_g_cur = find(aux_ga == g);
+      mat YG              = tcube_to_mat_by_slices( yt.slices(which_in_g_cur) );
+      mat XG              = tcube_to_mat_by_slices( xt.slices(which_in_g_cur) );
+    
+      field<mat> tmp_A_g_Sigma_g  = sample_A_c_Sigma_c( YG, XG, aux_A, aux_V, aux_Sigma, aux_nu );
+    
       aux_A_g.slice(g)            = tmp_A_g_Sigma_g(0);
       aux_Sigma_g.slice(g)        = tmp_A_g_Sigma_g(1);
       aux_Sigma_g_inv.slice(g)    = inv_sympd( aux_Sigma_g.slice(g) );
+      
     } // END g loop
     
     for (int c=0; c<C; c++) {
       aux_A_c.slice(c)            = aux_A_g.slice(aux_ga(c));
       aux_Sigma_c.slice(c)        = aux_Sigma_g.slice(aux_ga(c));
     } // END c loop
-    
-    
-    posterior_nu_S(s) = aux_nu;
     
     if (s % thin == 0) {
       posterior_A_c_cpp(ss)     = aux_A_c;
@@ -158,6 +167,7 @@ Rcpp::List bvarGroupPANEL(
       posterior_A.slice(ss)     = aux_A;
       posterior_V.slice(ss)     = aux_V;
       posterior_Sigma.slice(ss) = aux_Sigma;
+      posterior_ga.col(ss)      = aux_ga;
       posterior_nu(ss)          = aux_nu;
       posterior_m(ss)           = aux_m;
       posterior_w(ss)           = aux_w;
@@ -194,7 +204,8 @@ Rcpp::List bvarGroupPANEL(
       _["m"]        = posterior_m,
       _["w"]        = posterior_w,
       _["s"]        = posterior_s,
-      _["scale"]    = scale
+      _["scale"]    = scale,
+      _["group_allocation"] = posterior_ga + 1
     )
   );
 } // END bvarGroupPANEL
