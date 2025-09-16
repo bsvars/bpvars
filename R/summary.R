38749f6cc3848248e16c888d8ad4e5dc06b569b8
Tomasz Woźniak <wozniak.tom@pm.me>
2025-09-16 23:01:53 +1000

    summary for BVARGROUPPRIORPANEL #34 #30
    also no global in summary!

--- R/summary.R
+++ R/summary.R
@@ -13,7 +13,7 @@
 #' @param ... additional arguments affecting the summary produced.
 #' 
 #' @return A list reporting the posterior mean, standard deviations, as well as 5 and 95 
-#' percentiles of the country-specific and global parameters.
+#' percentiles of the country-specific parameters.
 #' 
 #' @method summary PosteriorBVARPANEL
 #' 
@@ -109,82 +109,6 @@ summary.PosteriorBVARPANEL = function(
   
   names(out) = country_names
   
-  
-  # global parameter summary
-  out_g         = list()
-  out_g$A       = list()
-  out_g$Sigma   = list()
-  out_g$V       = list()
-  out_g$hyper   = list()
-  
-  for (n in 1:N) {
-    
-    Sigma             = matrix(object$posterior$Sigma[n,1:n,], nrow = n)
-    out_g$Sigma[[n]]  = matrix(
-      cbind(
-        apply(Sigma, 1, mean),
-        apply(Sigma, 1, sd),
-        apply(Sigma, 1, quantile, probs = 0.05),
-        apply(Sigma, 1, quantile, probs = 0.95)
-      ),
-      ncol = 4
-    )
-    colnames(out_g$Sigma[[n]]) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_g$Sigma[[n]]) = paste0("Sigma[", n, ",", 1:n, "]")  
-    
-    A      = object$posterior$A[,n,]  
-    out_g$A[[n]] = cbind(
-      apply(A, 1, mean),
-      apply(A, 1, sd),
-      apply(A, 1, quantile, probs = 0.05),
-      apply(A, 1, quantile, probs = 0.95)
-    )
-    colnames(out_g$A[[n]]) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_g$A[[n]]) = Anames
-    
-  } # END n loop
-  
-  names(out_g$Sigma)  = paste0("equation", 1:N)
-  names(out_g$A)      = paste0("equation", 1:N)
-  
-  
-  for (k in 1:K) {
-    
-    V             = matrix(object$posterior$V[k,1:k,], nrow = k)
-    out_g$V[[k]]  = matrix(
-      cbind(
-        apply(V, 1, mean),
-        apply(V, 1, sd),
-        apply(V, 1, quantile, probs = 0.05),
-        apply(V, 1, quantile, probs = 0.95)
-      ),
-      ncol = 4
-    )
-    colnames(out_g$V[[k]]) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_g$V[[k]]) = paste0("V[", k, ",", 1:k, "]")  
-    
-  } # END k loop
-  
-  hyper         = t(cbind(
-    object$posterior$nu,
-    object$posterior$m,
-    object$posterior$w,
-    object$posterior$s
-  ))
-  out_g$hyper  = matrix(
-    cbind(
-      apply(hyper, 1, mean),
-      apply(hyper, 1, sd),
-      apply(hyper, 1, quantile, probs = 0.05),
-      apply(hyper, 1, quantile, probs = 0.95)
-    ),
-    ncol = 4
-  )
-  colnames(out_g$hyper) = c("mean", "sd", "5% quantile", "95% quantile")
-  rownames(out_g$hyper) = c("nu", "m", "w", "s")
-  
-  out$global = out_g
-  
   return(out)
 } # END summary.PosteriorBVARPANEL
 
@@ -206,7 +130,7 @@ summary.PosteriorBVARPANEL = function(
 #' @param ... additional arguments affecting the summary produced.
 #' 
 #' @return A list reporting the posterior mean, standard deviations, as well as 5 and 95 
-#' percentiles of the country-specific and global parameters.
+#' percentiles of the country-specific parameters.
 #' 
 #' @method summary PosteriorBVARGROUPPANEL
 #' 
@@ -306,84 +230,140 @@ summary.PosteriorBVARGROUPPANEL = function(
   
   names(out) = country_names
   
-  
-  # global parameter summary
-  out_g         = list()
-  out_g$A       = list()
-  out_g$Sigma   = list()
-  out_g$V       = list()
-  out_g$hyper   = list()
-  
-  for (n in 1:N) {
-    
-    Sigma             = matrix(object$posterior$Sigma[n,1:n,], nrow = n)
-    out_g$Sigma[[n]]  = matrix(
-      cbind(
-        apply(Sigma, 1, mean),
-        apply(Sigma, 1, sd),
-        apply(Sigma, 1, quantile, probs = 0.05),
-        apply(Sigma, 1, quantile, probs = 0.95)
-      ),
-      ncol = 4
-    )
-    colnames(out_g$Sigma[[n]]) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_g$Sigma[[n]]) = paste0("Sigma[", n, ",", 1:n, "]")  
-    
-    A      = object$posterior$A[,n,]  
-    out_g$A[[n]] = cbind(
-      apply(A, 1, mean),
-      apply(A, 1, sd),
-      apply(A, 1, quantile, probs = 0.05),
-      apply(A, 1, quantile, probs = 0.95)
-    )
-    colnames(out_g$A[[n]]) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_g$A[[n]]) = Anames
-    
-  } # END n loop
-  
-  names(out_g$Sigma)  = paste0("equation", 1:N)
-  names(out_g$A)      = paste0("equation", 1:N)
-  
-  
-  for (k in 1:K) {
-    
-    V             = matrix(object$posterior$V[k,1:k,], nrow = k)
-    out_g$V[[k]]  = matrix(
-      cbind(
-        apply(V, 1, mean),
-        apply(V, 1, sd),
-        apply(V, 1, quantile, probs = 0.05),
-        apply(V, 1, quantile, probs = 0.95)
-      ),
-      ncol = 4
-    )
-    colnames(out_g$V[[k]]) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_g$V[[k]]) = paste0("V[", k, ",", 1:k, "]")  
-    
-  } # END k loop
-  
-  hyper         = t(cbind(
-    object$posterior$nu,
-    object$posterior$m,
-    object$posterior$w,
-    object$posterior$s
-  ))
-  out_g$hyper  = matrix(
-    cbind(
-      apply(hyper, 1, mean),
-      apply(hyper, 1, sd),
-      apply(hyper, 1, quantile, probs = 0.05),
-      apply(hyper, 1, quantile, probs = 0.95)
-    ),
-    ncol = 4
-  )
-  colnames(out_g$hyper) = c("mean", "sd", "5% quantile", "95% quantile")
-  rownames(out_g$hyper) = c("nu", "m", "w", "s")
-  
-  out$global = out_g
-  
   return(out)
 } # END summary.PosteriorBVARGROUPPANEL
+
+
+
+
+
+
+
+
+
+
+
+
+#' @title Provides posterior estimation summary for Bayesian Hierarchical Panel 
+#' Vector Autoregressions with group-specific global prior
+#'
+#' @description Provides posterior mean, standard deviations, as well as 5 and 95 
+#' percentiles of the parameters for all \code{C} countries.
+#' 
+#' @param object an object of class \code{PosteriorBVARGROUPPRIORPANEL} obtained using the
+#' \code{estimate()} function applied to  
+#' Vector Autoregressions containing draws from the  posterior distribution of 
+#' the parameters. 
+#' @param ... additional arguments affecting the summary produced.
+#' 
+#' @return A list reporting the posterior mean, standard deviations, as well as 5 and 95 
+#' percentiles of the country-specific parameters.
+#' 
+#' @method summary PosteriorBVARGROUPPRIORPANEL
+#' 
+#' @seealso \code{\link{estimate.BVARGROUPPRIORPANEL}}, \code{\link{specify_bvarGroupPriorPANEL}}
+#'
+#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
+#' 
+#' @examples
+#' # specify the model
+#' specification = specify_bvarGroupPriorPANEL$new(
+#'       data = ilo_dynamic_panel,
+#'       group_allocation = country_grouping_region
+#' )
+#' burn_in       = estimate(specification, 10)             # run the burn-in
+#' posterior     = estimate(burn_in, 10)                   # estimate the model
+#' summary(posterior)
+#' 
+#' # workflow with the pipe |>
+#' ############################################################
+#' ilo_dynamic_panel |>
+#'   specify_bvarGroupPriorPANEL$new(
+#'         exogenous = ilo_exogenous_variables, 
+#'         group_allocation = country_grouping_region) |>
+#'   estimate(S = 10) |> 
+#'   estimate(S = 10) |> 
+#'   summary()
+#' 
+#' @export
+summary.PosteriorBVARGROUPPRIORPANEL = function(
+    object,
+    ...
+) {
+  
+  S         = dim(object$posterior$A_c)[4]
+  C         = dim(object$posterior$A_c)[3]
+  N         = dim(object$posterior$A_c)[2]
+  K         = dim(object$posterior$A_c)[1]
+  p         = object$last_draw$p
+  d         = K - N * p
+  
+  out       = list()
+  param     = c("A", "Sigma")
+  country_names = names(object$last_draw$data_matrices$Y)
+  
+  # country-specific parameter summary
+  for (c in 1:C) {
+    
+    out_c         = list()
+    out_c$Sigma   = list()
+    out_c$A       = list()
+    
+    for (n in 1:N) {
+      
+      Sigma_c       = matrix(object$posterior$Sigma_c[n,1:n,c,], nrow = n)
+      out_c$Sigma[[n]] = matrix(
+        cbind(
+          apply(Sigma_c, 1, mean),
+          apply(Sigma_c, 1, sd),
+          apply(Sigma_c, 1, quantile, probs = 0.05),
+          apply(Sigma_c, 1, quantile, probs = 0.95)
+        ),
+        ncol = 4
+      )
+      colnames(out_c$Sigma[[n]]) = c("mean", "sd", "5% quantile", "95% quantile")
+      rownames(out_c$Sigma[[n]]) = paste0("Sigma[", n, ",", 1:n, "]")  
+      
+      A_c      = object$posterior$A_c[,n,c,]  
+      out_c$A[[n]] = cbind(
+        apply(A_c, 1, mean),
+        apply(A_c, 1, sd),
+        apply(A_c, 1, quantile, probs = 0.05),
+        apply(A_c, 1, quantile, probs = 0.95)
+      )
+      colnames(out_c$A[[n]]) = c("mean", "sd", "5% quantile", "95% quantile")
+      
+      Anames  = c(
+        paste0(
+          rep("lag", p * N),
+          kronecker((1:p), rep(1, N)),
+          rep("_var", p * N),
+          kronecker((1:N), rep(1, p))
+        ),
+        "const"
+      )
+      if (d > 1) {
+        Anames = c(Anames, paste0("exo", 1:(d - 1)))
+      }
+      rownames(out_c$A[[n]]) = Anames
+    } # END n loop
+    
+    names(out_c$Sigma) = paste0("equation", 1:N)
+    names(out_c$A) = paste0("equation", 1:N)
+    
+    out[[c]]  = out_c
+  } # END c loop
+  
+  names(out) = country_names
+  
+  return(out)
+} # END summary.PosteriorBVARGROUPPRIORPANEL
+
+
+
+
+
+
 
 
 
@@ -403,7 +383,7 @@ summary.PosteriorBVARGROUPPANEL = function(
 #' @param ... additional arguments affecting the summary produced.
 #' 
 #' @return A list reporting the posterior mean, standard deviations, as well as 5 and 95 
-#' percentiles of the country-specific and global parameters.
+#' percentiles of the country-specific parameters.
 #' 
 #' @method summary PosteriorBVARs
 #' 
@@ -493,25 +473,6 @@ summary.PosteriorBVARs = function(
     
     names(out_c$Sigma) = paste0("equation", 1:N)
     names(out_c$A) = paste0("equation", 1:N)
-    
-    hyper         = rbind(
-        object$posterior$nu[c,],
-        object$posterior$m[c,],
-        object$posterior$w[c,],
-        object$posterior$s[c,]
-      )
-    
-    out_c$hyper  = matrix(
-      cbind(
-        apply(hyper, 1, mean),
-        apply(hyper, 1, sd),
-        apply(hyper, 1, quantile, probs = 0.05),
-        apply(hyper, 1, quantile, probs = 0.95)
-      ),
-      ncol = 4
-    )
-    colnames(out_c$hyper) = c("mean", "sd", "5% quantile", "95% quantile")
-    rownames(out_c$hyper) = c("nu", "m", "w", "s")
     
     out[[c]]  = out_c
   } # END c loop
