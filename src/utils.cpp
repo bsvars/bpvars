c8ec10fe62cf4caaa0abfc269cfd5101a42807e6
Tomasz Woźniak <wozniak.tom@pm.me>
2025-09-20 21:41:18 +1000

    cpp more missing #34

--- src/utils.cpp
+++ src/utils.cpp
@@ -17,6 +17,27 @@ arma::mat tcube_to_mat_by_slices (
   return y.slice(0).t(); // a (T x C_g, N) matrix
 } // END tcube_to_mat_by_slices
 
+
+
+
+// [[Rcpp::interfaces(cpp)]]
+// [[Rcpp::export]]
+arma::mat field_to_mat (
+    const arma::field<arma::mat>&   Y,
+    const arma::uvec&               which
+) {
+  int G = which.n_elem;
+  int N = Y(0).n_cols;
+  
+  mat out(0, N);
+  for (int g=0; g<G; g++) {
+    out = join_cols( out, Y(which(g)) );
+  }
+  return out;
+} // END tcube_to_mat_by_slices
+
+
+
 // this function is based on function csample_num1 that was copied from the bsvars package 
 // from file src/sample.cpp on 2025-04-13 and modified
 // [[Rcpp::interfaces(cpp)]]
