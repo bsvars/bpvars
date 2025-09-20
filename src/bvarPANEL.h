bf28eb5ee844eb98ddce1fba9d7e7c332fe8197d
Tomasz Woźniak <wozniak.tom@pm.me>
2025-09-20 15:25:36 +1000

    cpp gibbs missing estimation #36

--- src/bvarPANEL.h
+++ src/bvarPANEL.h
@@ -9,14 +9,16 @@ using namespace arma;
 
 Rcpp::List bvarPANEL(
     const int&                    S,          // No. of posterior draws
-    const Rcpp::List&             Y,          // a C-list of T_cxN elements
-    const Rcpp::List&             X,          // a C-list of T_cxK elements
+    const Rcpp::List&             Y,          // a C-list of (T_c + p)xN elements
+    const Rcpp::List&             missing,    // a C-list of T_cxN elements - 1 for missing
+    const Rcpp::List&             exogenous,  // a C-list of (T_c + p)x(d+1) - with intercept
     const Rcpp::List&             prior,      // a list of priors
     const Rcpp::List&             starting_values, 
     const int                     thin, // introduce thinning
     const bool                    show_progress,
     const arma::vec&              adptive_alpha_gamma, // 2x1 vector with target acceptance rate and step size
-    const bool                    type_wozniak = true
+    const bool                    type_wozniak,
+    const int                     p           // autoregressive lag order
 );
 
 
